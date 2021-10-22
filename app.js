9793daef7d533ad4e795eff86d0ab83120d9d919
Spenser Hale <spenserhale@outlook.com>
2021-10-21 17:23:18 -0700

    feat(Project): a composition example

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const Header = () =>
-<header>
-    <h1>Scoreboard</h1>
-    <span className={'stats'}>Players: 1</span>
-</header>
+    <header>
+        <h1>Scoreboard</h1>
+        <span className={'stats'}>Players: 1</span>
+    </header>
+
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Spenser
+            </span>
+
+            <Counter />
+        </div>
+    );
+}
+
+const Counter = ()  =>
+    <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
+    </div>
+
+const App = () =>
+    <div className="scoreboard">
+        <Header />
+        <Player />
+    </div>
 
 ReactDOM.render(
-    <Header/>,
+    <App/>,
     document.getElementById('root')
 )
