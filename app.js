5781691258beab5cb311b4cf001783766c527d5b
Spenser Hale <spenserhale@outlook.com>
2021-10-21 17:35:12 -0700

    feat(Project): using porps to create reusable components

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-const Header = () =>
+const Header = (props) =>
     <header>
-        <h1>Scoreboard</h1>
-        <span className={'stats'}>Players: 1</span>
+        <h1>{props.title}</h1>
+        <span className={'stats'}>Players: {props.totalPlayers}</span>
     </header>
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Spenser
+                {props.name}
             </span>
 
-            <Counter />
+            <Counter score={props.score} />
         </div>
     );
 }
 
-const Counter = ()  =>
+const Counter = (props)  =>
     <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">35</span>
+        <span className="counter-score">{props.score}</span>
         <button className="counter-action increment"> + </button>
     </div>
 
 const App = () =>
     <div className="scoreboard">
-        <Header />
-        <Player />
+        <Header title={"Scoreboard"} totalPlayers={1} />
+        <Player name={"Spenser"} score={50} />
+        <Player name={"Nick"} score={40} />
+        <Player name={"John"} score={10} />
+        <Player name={"Haily"} score={25} />
     </div>
 
 ReactDOM.render(
